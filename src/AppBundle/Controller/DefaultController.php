6b64f181aaa97755b9db5ad7804ab32b400ffa8d
Dima <steemd@gmail.com>
2016-08-13 13:31:53 +0300

    create new demo data

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,14 +8,39 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
+    // 
+    //  Route("/", name="homepage")
+    //  
+    // public function indexAction(Request $request)
+    // {
+    //     // replace this example code with whatever you need
+    //     return $this->render('default/index.html.twig', array(
+    //         'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+    //     ));
+    // }
+
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="home")
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+        return $this->render('AppBundle::base.html.twig');
     }
+
+    /**
+     * @Route("/test", name="test")
+     */
+    public function testAction(Request $request)
+    {
+        return $this->render('AppBundle:default:test.html.twig');
+    }
+
+    /**
+     * @Route("/some", name="some")
+     */
+    public function someAction(Request $request)
+    {
+        return $this->render('AppBundle:default:some.html.twig');
+    }
+
 }
