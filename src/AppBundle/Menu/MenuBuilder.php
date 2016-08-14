2da6786e416f259b9c60c0b72edb60a97e3ee947
Dima <steemd@gmail.com>
2016-08-14 16:47:35 +0300

    create two colomn show in admin panel

---
+++ src/AppBundle/Menu/MenuBuilder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class MenuBuilder
+{
+    private $factory;
+
+    /**
+     * @param FactoryInterface $factory
+     */
+    public function __construct(FactoryInterface $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    public function createMainMenu(RequestStack $requestStack)
+    {
+        $menu = $this->factory->createItem('root');
+
+        $menu->addChild('Home', array('route' => 'home'))
+            ->addChild('Test', array('route' => 'test'));
+        // ... add more children
+
+        return $menu;
+    }
+}
